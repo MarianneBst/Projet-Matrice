58b0c30d7bf9009e20764e00d6607b7b5f6f3355
VincentLiuzzi <vincent.liuzzi@hotmail.fr>
2017-04-04 22:45:46 +0200

    Début prog main

--- main.cpp
+++ main.cpp
@@ -6,69 +6,66 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	try{
-		//Création des matrice int 3x3 M1 (nulle) et M2 (123456789)
-		cout << "Matrice nulle : " << endl;
-		CMatrice<int> M1(3,3);
-		M1.MATAfficherMatrice();
-		CMatrice<int> M2(M1);
-		M2.MATModifierElement(2,1,2);
-		M2.MATModifierElement(3,1,3);
-		M2.MATModifierElement(5,2,2);
-		M2.MATModifierElement(8,3,2);
-		M2.MATModifierElement(6,2,3);
-		M2.MATModifierElement(1,1,1);
-		M2.MATModifierElement(9,3,3);
-		M2.MATModifierElement(7,3,1);
-		M2.MATModifierElement(4,2,1);
 	
-		cout << "Matrice M2 : " << endl;
-		M2.MATAfficherMatrice();
+	try{
+		int uiCptArgs;
+		int uiCptMatrice;
+		CMatrice tMATTabMatrice[argc-1];
+		for(uiCptArgs=1;uiCptArgs<argc;uiCptArgs++) // On parcourt les arguments pour remplir de tableau
+		{
+		
+			CParser P1;
+			cout << argv[uiCptArgs] << endl; 
+			P1.PARLireFichier(argv[uiCptArgs]); //On parse le fichier en paramètre
+			CMatrice<double> MP(P1); //On rempli le tableau avec la Matrice issue du fichier
+			tMATTabMatrice[uiCptArgs]=MP;
 
-		CMatrice<int> Mt(M2.MATTransposee());
-		cout << "Matrice transposée de M2 : " << endl;
-		Mt.MATAfficherMatrice();
-
-		CParser P1;
-		P1.PARLireFichier("test1.txt");
-		CMatrice<double> MP(P1);
-		cout << "****************************** MATRICE ISSUE DU PARSER *******************************************" << endl;
-		MP.MATAfficherMatrice();
+			cout << "********************* MATRICE ISSUE DU PARSER *********************" << endl;
+			MP.MATAfficherMatrice();
+		}
 
 		
-		//début de l'algorithme
-		int c;
-		cout << "Saisir une valeur" << endl;
-		cin >> c;
-		cout << "Multiplication de M2 par ";
-		cout << c ; cout << endl;
-		CMatrice<int> M3(c * M2);
-		cout << "Matrice M3 " << endl;
-		M3.MATAfficherMatrice();
-		cout << "Soustraction de M2 par M3 " << endl;
-		CMatrice<int> M4(M2 - M3);
-		M4.MATAfficherMatrice();
-		cout << "Addition de M2 par M3 " << endl;
-		CMatrice<int> M5(M2 + M3);
-		M5.MATAfficherMatrice();
-		cout << "Division de M2 par ";
-		cout << c ; cout << endl;
-		CMatrice<int> M6(c / M2);
-		cout << "Matrice M6 " << endl;
-		M6.MATAfficherMatrice();
-		CMatrice<int> M7(3,3);
-		M7.MATModifierElement(1,1,1);
-		M7.MATModifierElement(1,2,2);
-		M7.MATModifierElement(1,3,3);
-		cout << "Matrice M7 " << endl;
-		M7.MATAfficherMatrice();
-		cout << "M7 * M2" << endl;
-		CMatrice<int> M8(M7 * M2);
-		M8.MATAfficherMatrice();
-		CMatrice<int> M9(M2 + M3 + M7);
-		M9.MATAfficherMatrice();
+			//début de l'algorithme
+			int c;
+			cout << "Saisir une valeur de type double (avec un . pour les decimales)" << endl;
+			cin >> c;
+
+			for(uiCptMatrice=0;uiCptMatrice<argc;uiCptMatrice++)
+			{
+				//Multiplication des matrices par c
+				cout << "Multiplication de M" << uiCptMatrice+1 << " par " << c << " : " << endl;
+				CMatrice<int> MTest(c * tMATTabMatrice[uiCptArgs]);
+				M3.MATAfficherMatrice();
+
+				//Division des matrices par c
+				cout << "Division de M" << uiCptMatrice+1 << " par " << c << " : " << endl;
+				CMatrice<int> MTest(c / M2);
+
+				cout << "Soustraction de M2 par M3 " << endl;
+				CMatrice<int> M4(M2 - M3);
+				M4.MATAfficherMatrice();
+				cout << "Addition de M2 par M3 " << endl;
+				CMatrice<int> M5(M2 + M3);
+				M5.MATAfficherMatrice();
+				cout << "Division de M2 par ";
+				cout << c ; cout << endl;
+				CMatrice<int> M6(c / M2);
+				cout << "Matrice M6 " << endl;
+				M6.MATAfficherMatrice();
+				CMatrice<int> M7(3,3);
+				M7.MATModifierElement(1,1,1);
+				M7.MATModifierElement(1,2,2);
+				M7.MATModifierElement(1,3,3);
+				cout << "Matrice M7 " << endl;
+				M7.MATAfficherMatrice();
+				cout << "M7 * M2" << endl;
+				CMatrice<int> M8(M7 * M2);
+				M8.MATAfficherMatrice();
+				CMatrice<int> M9(M2 + M3 + M7);
+				M9.MATAfficherMatrice();
 	}catch(Cexception EXCObjet){
 		EXCObjet.EXCLireMessage();	
+	}
 	}
 	return 0;
 }
